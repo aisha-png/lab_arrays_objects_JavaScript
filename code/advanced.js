d4bf8c4caaa44964e33fdb9343a476997df35807
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-30 16:28:28 +0100

    finished lab extensions

--- code/advanced.js
+++ code/advanced.js
@@ -42,7 +42,8 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-// england = england.touristAttractions["London Eye", "Big Ben", "Buckingham Palace"];
+let england = unitedKingdom[1];
+england.touristAttractions = ["London Eye", "Big Ben", "Buckingham Palace"];
 console.log(england);
 
 
@@ -51,9 +52,10 @@ console.log(england);
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
+let wales = unitedKingdom[2];
+wales.capital = "Cardiff";
 
-// console.log(wales);
+console.log(wales);
 
 
 
@@ -61,12 +63,57 @@ wales = null;
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
 
-// console.log(northernIrelandKeys);
+let northerIreland = unitedKingdom[3];
+northernIrelandKeys = Object.keys(northerIreland);
+console.log(northernIrelandKeys);
 
 
 
 /**
- * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
+ * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, 
+ * smallest or somewhere in the middle.
  */
+
+// very long method:
+
+scotland = unitedKingdom[0];
+england = unitedKingdom[1];
+wales = unitedKingdom[2];
+northernIreland = unitedKingdom[3];
+scotlandAndEnglandPopulationDifference = scotland.population - england.population;
+scotlandAndWalesPopulationDifference = scotland.population - wales.population;
+scotlandAndNorthernIrelandPopulationDifference = scotland.population - northerIreland.population;
+
+if (england.population > scotland.population){
+    console.log("England has a larger population by " + scotlandAndEnglandPopulationDifference);
+}
+else {
+    console.log("Scotland has a larger population by " + scotlandAndEnglandPopulationDifference);
+}
+
+if (wales.population > scotland.population){
+    console.log("Wales has a larger population by " + scotlandAndWalesPopulationDifference);
+}
+else {
+    console.log("Scotland has a larger population by " +  scotlandAndWalesPopulationDifference);
+}
+
+if (northerIreland.population > scotland.population){
+    console.log("Northern Ireland has a larger population by " + scotlandAndNorthernIrelandPopulationDifference);
+}
+else {
+    console.log("Scotland has a larger population by " + scotlandAndNorthernIrelandPopulationDifference);
+}
+
+// shorter method:
+
+if (scotland.population > england.population && wales.population && northerIreland.population){
+    console.log("Scotland has the largest population of " + scotland.population);
+}
+else if(scotland.population < england.population && wales.population && northerIreland.population){
+    console.log("Scotland has the lowest population");
+}
+else {
+    console.log("Scotland is larger than some cities and smaller than others");
+}
